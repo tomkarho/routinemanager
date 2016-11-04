3f2ed80d813a4a0d46f30333881a8d50abb6e685
tomkarho <tomkarho@gmail.com>
2016-11-04 12:39:40 +0200

    Diplay routine fragment

--- app/src/main/java/com/protacon/toka/routinemanager/ListActivity.java
+++ app/src/main/java/com/protacon/toka/routinemanager/ListActivity.java
@@ -1,7 +1,11 @@
 package com.protacon.toka.routinemanager;
 
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+
+import com.protacon.toka.routinemanager.fragments.RoutineFragment;
 
 public class ListActivity extends FragmentActivity {
 
@@ -9,5 +13,13 @@ public class ListActivity extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+
+        if (fragment == null) {
+            fragment = new RoutineFragment();
+            fm.beginTransaction().add(R.id.fragment_container, fragment).commit();
+        }
     }
 }
