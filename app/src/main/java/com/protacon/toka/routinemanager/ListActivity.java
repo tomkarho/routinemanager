767239a23a9a8348e81a267f72944ac4d896fcb4
tomkarho <tomkarho@gmail.com>
2016-11-04 12:00:41 +0200

    Change ListActivity type to FragmentActivity in order to support gfragments

--- app/src/main/java/com/protacon/toka/routinemanager/ListActivity.java
+++ app/src/main/java/com/protacon/toka/routinemanager/ListActivity.java
@@ -1,9 +1,10 @@
 package com.protacon.toka.routinemanager;
 
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class ListActivity extends AppCompatActivity {
+public class ListActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
