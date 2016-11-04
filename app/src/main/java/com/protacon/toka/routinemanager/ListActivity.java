6702770b2624a046fb76ea83bb2cd6b42d319fc2
tomkarho <tomkarho@gmail.com>
2016-11-04 14:47:01 +0200

    Abstract SingleFragmentActivity contains all the necessary code for different activities to implement fragments.

--- app/src/main/java/com/protacon/toka/routinemanager/ListActivity.java
+++ app/src/main/java/com/protacon/toka/routinemanager/ListActivity.java
@@ -7,19 +7,10 @@ import android.support.v4.app.FragmentManager;
 
 import com.protacon.toka.routinemanager.fragments.RoutineFragment;
 
-public class ListActivity extends FragmentActivity {
+public class ListActivity extends SingleFragmentActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_fragment);
-
-        FragmentManager fm = getSupportFragmentManager();
-        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
-
-        if (fragment == null) {
-            fragment = new RoutineFragment();
-            fm.beginTransaction().add(R.id.fragment_container, fragment).commit();
-        }
+    protected Fragment createFragment() {
+        return new RoutineFragment();
     }
 }
---
+++ app/src/main/java/com/protacon/toka/routinemanager/SingleFragmentActivity.java
@@ -0,0 +1,27 @@
+package com.protacon.toka.routinemanager;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+abstract class SingleFragmentActivity extends FragmentActivity {
+
+    protected abstract Fragment createFragment();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_fragment);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+
+        if (fragment == null) {
+            fragment = createFragment();
+            fm.beginTransaction().add(R.id.fragment_container, fragment).commit();
+        }
+    }
+}
