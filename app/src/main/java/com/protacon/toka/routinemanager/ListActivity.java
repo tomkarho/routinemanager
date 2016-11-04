ae75a6d31c86c6e1e65a179ab8c4a94bade4b04e
tomkarho <tomkarho@gmail.com>
2016-11-04 12:00:53 +0200

    Remove unused import

--- app/src/main/java/com/protacon/toka/routinemanager/ListActivity.java
+++ app/src/main/java/com/protacon/toka/routinemanager/ListActivity.java
@@ -1,7 +1,6 @@
 package com.protacon.toka.routinemanager;
 
 import android.support.v4.app.FragmentActivity;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class ListActivity extends FragmentActivity {
